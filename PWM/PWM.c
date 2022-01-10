fa3939806c21eedcbc21f6791cd57298e47713dd
AbdallaSaied <Abdallah.abdelhafez70@gmail.com>
2022-01-10 17:53:10 +0200

    Signed-off-by: AbdallaSaied <Abdallah.abdelhafez70@gmail.com>

--- PWM/PWM.c
+++ PWM/PWM.c
@@ -17,8 +17,8 @@ static uint8_t PWM_dutyCycle=0;
 void PWM_vidInit(void){
     SetCallBack_Timer0OVF(PWM_vidstart);
     TIMER_vidTimer0Init();
-    DIO_vidSetPinDirection(PWM_PORT1 , PWM_PIN1 , OUTPUT );
-    DIO_vidSetPinDirection(PWM_PORT2 , PWM_PIN2 , OUTPUT );
+    DIO_SETpinDir(PWM_PORT1 , PWM_PIN1 , OUTPUT );
+    DIO_SETpinDir(PWM_PORT2 , PWM_PIN2 , OUTPUT );
    // DIO_vidSetPinDirection(PWM_PORT3 , PWM_PIN3 , OUTPUT );
     TIMER_vidTimer0Start(PWM_INTIAL_TIMER_VALUE);
 }
@@ -29,16 +29,16 @@ void PWM_vidstart(void)
     PWM_timer_count++;
     if (PWM_timer_count == PWM_dutyCycle)
     {
-        DIO_vidSetPinValue(PWM_PORT1 , PWM_PIN1 , STD_LOW);
-        DIO_vidSetPinValue(PWM_PORT2 , PWM_PIN2 , STD_LOW);
+        DIO_SETpinVal(PWM_PORT1 , PWM_PIN1 , STD_LOW);
+        DIO_SETpinVal(PWM_PORT2 , PWM_PIN2 , STD_LOW);
        // DIO_vidSetPinValue(PWM_PORT3 , PWM_PIN3 , STD_LOW);
         
     }
     else if(PWM_timer_count >= 100)
     {
         PWM_timer_count = 0;
-        DIO_vidSetPinValue(PWM_PORT1 , PWM_PIN1 , STD_HIGH);
-        DIO_vidSetPinValue(PWM_PORT2 , PWM_PIN2 , STD_HIGH);
+        DIO_SETpinVal(PWM_PORT1 , PWM_PIN1 , STD_HIGH);
+        DIO_SETpinVal(PWM_PORT2 , PWM_PIN2 , STD_HIGH);
        // DIO_vidSetPinValue(PWM_PORT3 , PWM_PIN3 , STD_HIGH);
         
     }
