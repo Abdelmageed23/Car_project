d53ff05b7c6525b208dc10803865707835e718e3
YahiaElGamal <96386403+yahiaElGamal98@users.noreply.github.com>
2022-01-10 20:51:14 +0200

    Motor Driver V1.0 by Yahia ElGamal

---
+++ MOTOR_CONFIG.h
@@ -0,0 +1,25 @@
+/*
+ * MOTOR_CONFIG.h
+ *
+ * Created: 1/10/2022 8:11:03 PM
+ *  Author: No. 01
+ */ 
+
+
+#ifndef MOTOR_CONFIG_H_
+#define MOTOR_CONFIG_H_
+
+
+#include "DIO_interface.h"
+
+#define MOTOR_PORT DIO_PORTD
+
+#define MOTOR_DIR_1A DIO_PIN2
+#define MOTOR_DIR_1B DIO_PIN3
+
+#define MOTOR_DIR_2A DIO_PIN6
+#define MOTOR_DIR_2B DIO_PIN7
+
+
+
+#endif /* MOTOR_CONFIG_H_ */
---
+++ Motor.c
@@ -0,0 +1,74 @@
+/*
+ * Motor.c
+ *
+ * Created: 1/9/2022 5:56:57 PM
+ *  Author: No. 01
+ */ 
+#include "Motor.h"
+#include "MOTOR_CONFIG.h"
+#include "DIO_interface.h"
+
+
+void MOTOR_init(uint8_t numMotor)// numMotor (initialize Direction pins only (PWM initializes enable pins inside them))
+{
+	PWM_vidInit();
+	if(MOTOR_1==numMotor)
+	{
+		DIO_SETpinDir(MOTOR_PORT,MOTOR_DIR_1A,DIO_OUTPUT);
+		DIO_SETpinDir(MOTOR_PORT,MOTOR_DIR_1B,DIO_OUTPUT);
+	}
+	else if(MOTOR_2==numMotor)
+	{
+		DIO_SETpinDir(MOTOR_PORT,MOTOR_DIR_2A,DIO_OUTPUT);
+		DIO_SETpinDir(MOTOR_PORT,MOTOR_DIR_2B,DIO_OUTPUT);
+	}
+}
+
+MOTOR_speed(uint8_t dutyCycle)//  30/60/90 (Start PWM and initialize duty cycle)
+{
+	PWM_SetDutyCycle(dutyCycle);
+}
+MOTOR_direction(uint8_t numMotor, uint8_t DIR)//  numMotor/(FWD,REV) (Set direction of specified motor) i.e (MOTOR_1, FWD) meaning motor 1, move forward
+{
+	if(MOTOR_1==numMotor)
+	{
+		if(FWD==DIR)
+		{
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1A,DIO_HIGH);
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1B,DIO_LOW);
+		}
+		else if(REV==DIR)
+		{
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1A,DIO_LOW);
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1B,DIO_HIGH);
+		}
+	}
+	else if(MOTOR_2==numMotor)
+	{
+		if(FWD==DIR)
+		{
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2A,DIO_HIGH);
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2B,DIO_LOW);
+		}
+		else if(REV==DIR)
+		{
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2A,DIO_LOW);
+			DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2B,DIO_HIGH);
+		}
+	}
+	
+	
+}
+MOTOR_off(uint8_t numMotor)// stop specified motor (writes LOW on DIR data pins)
+{
+	if(MOTOR_1==numMotor)
+	{
+		DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1A,DIO_LOW);
+		DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_1B,DIO_LOW);
+	}
+	else if(MOTOR_2==numMotor)
+	{
+		DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2A,DIO_LOW);
+		DIO_SETpinVal(MOTOR_PORT,MOTOR_DIR_2B,DIO_LOW);
+	}
+}
