81203f3362f040b243b5a34787653bfb3bdc7bb0
AbdallaSaied <Abdallah.abdelhafez70@gmail.com>
2022-01-10 17:23:26 +0200

    Signed-off-by: AbdallaSaied <Abdallah.abdelhafez70@gmail.com>

---
+++ PWM/PWM.c
@@ -0,0 +1,53 @@
+/**********************************/
+/* Author   : Abdalla Abdelhafiz  */
+/* Date     : 8/1/2022            */
+/* Version  : V01                 */
+/**********************************/
+#include "../STD_TYPES.h"
+#include "../BIT_MATH.h"
+#include "../TIMER/Timer.h"
+#include "../DIO/Dio.h"
+#include "PWM.h"
+#include "PWMconfig.h"
+
+
+static uint8_t PWM_timer_count = 0;
+static uint8_t PWM_dutyCycle=0;
+
+void PWM_vidInit(void){
+    TIMER_vidTimer0Init();
+    DIO_vidSetPinDirection(PWM_PORT1 , PWM_PIN1 , OUTPUT );
+    DIO_vidSetPinDirection(PWM_PORT2 , PWM_PIN2 , OUTPUT );
+   // DIO_vidSetPinDirection(PWM_PORT3 , PWM_PIN3 , OUTPUT );
+    TIMER_vidTimer0Start(PWM_INTIAL_TIMER_VALUE);
+}
+
+// the callback function when the timer overflow 
+void PWM_vidstart(void)
+{
+    PWM_timer_count++;
+    if (PWM_timer_count == PWM_dutyCycle)
+    {
+        DIO_vidSetPinValue(PWM_PORT1 , PWM_PIN1 , STD_LOW);
+        DIO_vidSetPinValue(PWM_PORT2 , PWM_PIN2 , STD_LOW);
+       // DIO_vidSetPinValue(PWM_PORT3 , PWM_PIN3 , STD_LOW);
+        
+    }
+    else if(PWM_timer_count >= 100)
+    {
+        PWM_timer_count = 0;
+        DIO_vidSetPinValue(PWM_PORT1 , PWM_PIN1 , STD_HIGH);
+        DIO_vidSetPinValue(PWM_PORT2 , PWM_PIN2 , STD_HIGH);
+       // DIO_vidSetPinValue(PWM_PORT3 , PWM_PIN3 , STD_HIGH);
+        
+    }
+    else
+    {
+        // do nothing
+    }
+}
+
+//function to change dutycycle
+void PWM_SetDutyCycle(uint8_t dutyc){
+    PWM_dutyCycle = dutyc;
+}
---
+++ PWM/PWMconfig.h
@@ -0,0 +1,19 @@
+
+/**********************************/
+/* Author   : Abdalla Abdelhafiz  */
+/* Date     : 8/1/2022            */
+/* Version  : V01                 */
+/**********************************/
+#ifndef PWM_CONFIG_H_
+#define PWM_CONFIG_H_
+
+#define PWM_PORT1    DIO_PORTD
+#define PWM_PORT2    DIO_PORTD
+#define PWM_PORT3    DIO_PORTB
+#define PWM_PIN1     DIO_PIN4
+#define PWM_PIN2     DIO_PIN5
+#define PWM_PIN3     DIO_PIN7
+
+#define PWM_INTIAL_TIMER_VALUE   231
+
+#endif
